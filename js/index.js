c29edf714d36a451a33871357e0961eb3449e6fe
KillSwitch <asd>
2020-05-26 13:47:26 +0200

    Added automatic theme displaying on load

--- js/index.js
+++ js/index.js
@@ -2,13 +2,26 @@ const LIGHT_THEME = "light";
 const DARK_THEME = "dark";
 const HTML = document.documentElement;
 
-function toggleTheme() {
+document.body.onload = function() {
+    displayTheme(); // Display current theme
+    console.log(getCookie("theme"));
+}
+
+function displayTheme(toggle=false) {
     var current_theme = getCookie("theme") == "" ? "light" : getCookie("theme"); // If the cookie is null, treat it as if it were light
     var toggled_theme = current_theme == LIGHT_THEME ? DARK_THEME : LIGHT_THEME; // Decide what the toggled theme should be
     
-    createCookie("theme", toggled_theme);
     HTML.classList.remove(LIGHT_THEME, DARK_THEME) // Reset the theme
-    HTML.classList.add(toggled_theme)
+    // Toggle and display
+    if(toggle) {
+        createCookie("theme", toggled_theme);
+        HTML.classList.add(toggled_theme);
+    }
+    // Just display
+    else
+        HTML.classList.add(current_theme);
+
+    
 }
 
 // An optimized function for getting cookies
@@ -29,6 +42,6 @@ function getCookie(cname) {
 }
 
 // Creates a cookie or overwrites an existing one
-function createCookie(cname, cvalue, expire = "never") {
+function createCookie(cname, cvalue, expire = "10000") {
     document.cookie = `${cname}=${cvalue};expires=${expire};path=/`
 }
